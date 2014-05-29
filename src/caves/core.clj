91b6d830bef7b895c700d1f96c56dd4f573c605c
Jacob Michelsen <chromegadget@gmail.com>
2014-05-29 23:29:44 +0200

    After part 3.3, scrolling and crosshair added

--- src/caves/core.clj
+++ src/caves/core.clj
@@ -31,21 +31,55 @@
   (s/put-string screen 0 0 "Sorry, better luck next time.")
   (s/put-string screen 0 1 "Press escape to exit, anything else to go."))
 
-(defmethod draw-ui :play [ui {{:keys [tiles]} :world :as game} screen]
-  (let [[cols rows] screen-size
-       vcols cols
-       vrows (dec rows)
-       start-x 0
-       start-y 0
-       end-x (+ start-x vcols)
-       end-y (+ start-y vrows)]
+(defn draw-world [screen vrows vcols start-x start-y end-x end-y tiles]
   (doseq [[vrow-idx mrow-idx] (map vector
                                    (range 0 vrows)
                                    (range start-y end-y))
           :let [row-tiles (subvec (tiles mrow-idx) start-x end-x)]]
     (doseq [vcol-idx (range vcols)
             :let [{:keys [glyph color]} (row-tiles vcol-idx)]]
-      (s/put-string screen vcol-idx vrow-idx glyph {:fg color})))))
+      (s/put-string screen vcol-idx vrow-idx glyph {:df color}))))
+
+(defn draw-crosshairs [screen vcols vrows]
+  (let [crosshair-x (int (/ vcols 2))
+        crosshair-y (int (/ vrows 2))]
+    (s/put-string screen crosshair-x crosshair-y "X" {:fg :red})
+    (s/move-cursor screen crosshair-x crosshair-y)))
+
+(defn get-viewport-coords [game vcols vrows]
+  (let [location (:location game)
+       [center-x center-y] location
+
+       tiles (:tiles (:world game))
+       map-rows (count tiles)
+       map-cols (count (first tiles))
+
+       start-x (- center-x (int (/ vcols 2)))
+       start-x (max 0 start-x)
+       start-y (- center-y (int (/ vrows 2)))
+       start-y (max 0 start-y)
+
+       end-x (+ start-x vcols)
+       end-x (min end-x map-cols)
+
+       end-y (+ start-y vrows)
+       end-y (min end-y map-rows)
+
+       start-x (- end-x vcols)
+       start-y (- end-y vrows)]
+
+    [start-x start-y end-x end-y]))
+
+;; :(defmethod draw-ui :play [ui {{:keys [tiles]} :world :as game} screen]
+  (defmethod draw-ui :play [ui game screen]
+    (let [world (:world game)
+         tiles (:tiles world)
+       [cols rows] screen-size
+       vcols cols
+       vrows (dec rows)
+       [start-x start-y end-x end-y] (get-viewport-coords game vcols vrows)]
+    (draw-world screen vrows vcols start-x start-y end-x end-y tiles)
+    (draw-crosshairs screen vcols vrows)))
 
 (defn draw-game [game screen]
   (clear-screen screen)
@@ -73,11 +107,25 @@
     (assoc game :uis [])
     (assoc game :uis [(new UI :start)])))
 
+(defn move [[x y] [dx dy]]
+  [(+ x dx) (+ y dy)])
+
 (defmethod process-input :play [game input]
   (case input
     :enter (assoc game :uis [(new UI :win)])
     :backspace (assoc game :uis [(new UI :lose)])
+    \q         (assoc game :uis [])
     \s         (assoc game :world (smooth-world (:world game)))
+    \h (update-in game [:location] move [-1 0])
+    \j (update-in game [:location] move [0 1])
+    \k (update-in game [:location] move [0 -1])
+    \l (update-in game [:location] move [1 0])
+
+    \H (update-in game [:location] move [-5 0])
+    \J (update-in game [:location] move [0 5])
+    \K (update-in game [:location] move [0 -5])
+    \L (update-in game [:location] move [5 0])
+
     game))
 
 (defn get-input [game screen]
@@ -93,7 +141,8 @@
         (recur (process-input (dissoc game :input) input))))))
 
 (defn new-game []
-  (new Game nil [(new UI :start)] nil))
+  (assoc (new Game nil [(new UI :start)] nil)
+  :location [40 20]))
 
 (defn main
   ([screen-type] (main screen-type false))
